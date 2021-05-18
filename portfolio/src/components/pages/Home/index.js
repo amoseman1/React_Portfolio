7130cae08a7e25d4509c69135f6fc0c816aa669b
Waterbug <amoseman18@gmail.com>
2021-05-18 09:18:04 -0600

    changed home page layout to grid

--- portfolio/src/components/pages/Home/index.js
+++ portfolio/src/components/pages/Home/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import './homestyle.css';
-// import Image from 'react-bootstrap/Image'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+import Image from 'react-bootstrap/Image'
 
 
 
@@ -14,9 +17,11 @@ function Home() {
             <br />
             <br />
 
-            <div className="media">
-                <img id="photo" src="./IMG_1911.jpg" alt="author"></img>
-                <main id="about">Adventurer by heart, leader and problem solver by nature. I’ve worn many
+            <Row className="justify-content-lg-center">
+                {/* <Container> */}
+                <Col lg={3}>
+                    <Image id="photo" src="./IMG_1911.jpg" alt="author"></Image></Col>
+                <Col lg={9}> <main id="about">Adventurer by heart, leader and problem solver by nature. I’ve worn many
                 hats in my life which have given me the life lessons and experiences to mold
                 me into a very adaptable and hard-working individual. Spending most of my
                 working life in the hospitality industry, I know what is to be punctual,
@@ -26,10 +31,15 @@ function Home() {
                 to be an influencer with integrity, an enthusiastic motivator, and an organized
                 and dependable communicator. All of these traits have made my transition
                 into a web developer much smoother. The daily collaboration is a crucial part
-                of the experience. I can't wait to build some amazing things together.</main>
-            </div>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/9/94/MERN-logo.png" id="mern" alt="mern"></img>
+                of the experience. I can't wait to build some amazing things together.</main></Col>
 
+                {/* </Container> */}
+            </Row>
+            <Row className="justify-content-lg-center">
+                <Col>
+                    <Image src="https://upload.wikimedia.org/wikipedia/commons/9/94/MERN-logo.png" id="mern" alt="mern"></Image>
+                </Col>
+            </Row>
         </>
     );
 }
