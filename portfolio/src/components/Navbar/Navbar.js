577ffe2aa101b3bd593eb772c17b8d939933b3c9
Waterbug <amoseman18@gmail.com>
2021-05-17 18:30:35 -0600

    fixed home page styling

--- portfolio/src/components/Navbar/Navbar.js
+++ portfolio/src/components/Navbar/Navbar.js
@@ -17,13 +17,15 @@ function Navbar() {
                         <nav className="uk-navbar-container uk-navbar-transparent" uk-navbar>
                             <div className="uk-navbar-left">
                                 <ul className="uk-navbar-nav">
+                                    <li>
+                                        <Link to="/" id="name" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>Amy Moseman .
+                                        </Link>
+                                    </li>
                                     <li className="uk-active">
-                                        {/* <a id="navList" href="index.html">Home</a> */}
                                         <Link to="/" id="navList" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>  Home
                                         </Link>
                                     </li>
                                     <li>
-                                        {/* <a id="navList" href="portfolio.html">Portfolio</a> */}
                                         <Link to="/portfolio2" id="navList" className={location.pathname === "/portfolio2" ? "nav-link active" : "nav-link"}>Portfolio
                                         </Link>
                                     </li>
@@ -31,10 +33,9 @@ function Navbar() {
                                         <Link to="/contact" id="navList" className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}>Contact
                                        </Link>
                                     </li>
-
-                                    {/* just put this in to see if it works need to ttest!!!! */}
-                                    <li><a href="https://docs.google.com/document/d/1tbBlPYtNdwyHWxWVoBnkWIAG3jI65zqGx4CuFJDmJd4/edit?usp=sharing" rel="noreferrer" target="_blank" id="navList">Resume</a></li>
-
+                                    <li>
+                                        <a href="https://docs.google.com/document/d/1tbBlPYtNdwyHWxWVoBnkWIAG3jI65zqGx4CuFJDmJd4/edit?usp=sharing" rel="noreferrer" target="_blank" id="navList">Resume</a>
+                                    </li>
                                 </ul>
                             </div>
                         </nav>
