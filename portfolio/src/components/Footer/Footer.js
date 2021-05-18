139fffacf93468085bb93b80c78af1f17e4663c5
Waterbug <amoseman18@gmail.com>
2021-05-17 19:48:53 -0600

    changed homepage again, adding content to rid whitespace at bottom of page

--- portfolio/src/components/Footer/Footer.js
+++ portfolio/src/components/Footer/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
             </ul>
 
         </div>
-        <p id="copywright">&copy;Amy Moseman 2021</p>
+        {/* <p id="copywright">&copy;Amy Moseman 2021</p> */}
     </>
     )
 }
--- portfolio/src/components/pages/Home/index.js
+++ portfolio/src/components/pages/Home/index.js
@@ -1,12 +1,22 @@
 import React from "react";
 import './homestyle.css';
+// import Image from 'react-bootstrap/Image'
+
+
+
 
 function Home() {
     return (
         <>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+
             <div className="media">
                 <img id="photo" src="./IMG_1911.jpg" alt="author"></img>
-                <p id="about">Adventurer by heart, leader and problem solver by nature. I’ve worn many
+                <main id="about">Adventurer by heart, leader and problem solver by nature. I’ve worn many
                 hats in my life which have given me the life lessons and experiences to mold
                 me into a very adaptable and hard-working individual. Spending most of my
                 working life in the hospitality industry, I know what is to be punctual,
@@ -16,8 +26,10 @@ function Home() {
                 to be an influencer with integrity, an enthusiastic motivator, and an organized
                 and dependable communicator. All of these traits have made my transition
                 into a web developer much smoother. The daily collaboration is a crucial part
-                of the experience. I can't wait to build some amazing things together.</p>
+                of the experience. I can't wait to build some amazing things together.</main>
             </div>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/9/94/MERN-logo.png" id="mern" alt="mern"></img>
+
         </>
     );
 }
